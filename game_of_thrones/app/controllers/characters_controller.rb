18503693b131071c84e546ea6649e5aeeecb4f07
Donovan Allen <dallen.928@gmail.com>
2016-11-07 15:13:37 -0500

    finishes mvp, starts styling

--- game_of_thrones/app/controllers/characters_controller.rb
+++ game_of_thrones/app/controllers/characters_controller.rb
@@ -1,2 +1,31 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  def new
+    @character = Character.new
+  end
+
+  def create
+    @character = Character.create!(character_params)
+    redirect_to @character
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path
+  end
+
+
+private
+
+  def character_params
+    params.require(:character).permit(:name, :age)
+  end
 end
--- game_of_thrones/app/controllers/houses_controller.rb
+++ game_of_thrones/app/controllers/houses_controller.rb
@@ -1,2 +1,31 @@
 class HousesController < ApplicationController
+  def index
+    @houses = House.all
+  end
+
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to @house
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    redirect_to houses_path
+  end
+
+
+private
+
+  def house_params
+    params.require(:house).permit(:name, :banner, :creed)
+  end
 end
